e46f9ad393cceace5a24c196ea622b872e756fe2
Connor Cowan <ccowan003@comcast.net>
2021-07-30 09:48:15 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const url = "https://distinct-vaulted-freesia.glitch.me/image";
-const fgTitle = document.getElementById("fg-title")
-const fgImage = document.getElementById("fg-image")
+const fgTitle = document.getElementById("fg-title");
+const fgImage = document.getElementById("fg-image");
 const fgLikes = document.getElementById("fg-likes")
 const likeButton = document.getElementById("like-button")
 const fgComments = document.getElementById("fg-comments")
@@ -8,27 +8,29 @@ const commentForm = document.getElementById("comment-form")
 
 commentForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(addComment(event.target.comment.value));
+    (renderComment(event.target.comment.value));
     event.target.comment.value = '';
 });
 
 fetch(url)
-.then(res => res.json())
-.then(data => handlePage(data));
+.then((res) => res.json())
+.then(renderPage);
+//.then((data) => renderPage(data));
 
-function handlePage(data) {
+function renderPage(data) {
     fgImage.src = (data.image);
     fgTitle.innerText = (data.title);
     likeButton.addEventListener('click', () => {
-        data.likes === 25
-        data.likes++
-        fgLikes.innerText = `${data.likes} likes`
+        //data.likes++;
+        fgLikes.innerText = `${data.likes++} likes`
     });
-    setComment(data.comments);
+    renderComment(data.comments);
 }
-function setComment(comments) {
+
+function renderComment(comments) {
     fgComments.innerHTML = "";          //empty comments 
     comments.forEach((comment) => addComment(comment.content));
+    //forEach comment cb & append the content
 }
 
 function addComment(comment) {
