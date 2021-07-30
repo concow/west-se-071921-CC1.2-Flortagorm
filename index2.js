3353b3213ae6f6170c004212ad1c686d0d1d9b0c
Connor Cowan <ccowan003@comcast.net>
2021-07-30 12:42:00 -0700

    Refactor

--- index2.js
+++ index2.js
@@ -6,13 +6,6 @@ const likeButton = document.getElementById("like-button")
 const fgComments = document.getElementById("fg-comments")
 const commentForm = document.getElementById("comment-form")
 
-let likes = 0;
-
-likeButton.addEventListener('click', () => {
-    likes++;
-    renderLikes();
-});
-
 commentForm.addEventListener('submit', (event) => {
     event.preventDefault();
     addComment(event.target.comment.value);
@@ -22,17 +15,19 @@ commentForm.addEventListener('submit', (event) => {
 fetch(url)
     .then((res) => res.json())
     .then(renderPage);
-    //.then((data) => renderPage(data));
+//.then((data) => renderPage(data));
 
 function renderPage(data) {
     fgImage.src = (data.image);
     fgTitle.innerText = (data.title);
-    likes = data.likes;
-    renderComments(data.comments);
-    renderLikes();
-    }
+    likeButton.addEventListener('click', () => {
+        //data.likes++;
+        fgLikes.innerText = `${data.likes++} likes`
+    });
+    setComments(data.comments);
+}
 
-function renderComments(comments) {
+function setComments(comments) {
     fgComments.innerHTML = "";          //empty comments 
     comments.forEach((comment) => addComment(comment.content));
 }
@@ -41,8 +36,4 @@ function addComment(comment) {
     const li = document.createElement('li');
     li.innerText = comment;
     fgComments.append(li);
-}
-
-function renderLikes() {
-    fgLikes.innerText = `${likes} likes`
 }
